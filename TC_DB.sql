aca1d8e9a4750bfea866de68e50e538224190949
Danii <daniel.marin92@hotmail.com>
2024-02-03 21:17:51 -0600

    fix: Data types DB script

--- TC_DB.sql
+++ TC_DB.sql
@@ -1,40 +1,41 @@
-CREATE DATABASE "TECH_CONSULTING" IF NOT EXISTS;
+-- Este comando se debe ejecutar fuera de este script SQL, ya que PostgreSQL no permite condicionales en la creación de DB directamente.
+-- CREATE DATABASE "TECH_CONSULTING";
 
-USE "TECH_CONSULTING";
+DROP TABLE IF EXISTS "compras_servicios";
+DROP TABLE IF EXISTS "compras";
+DROP TABLE IF EXISTS "servicios";
+DROP TABLE IF EXISTS "usuarios";
 
-
-DROP TABLE "usuarios" IF EXISTS;
 CREATE TABLE "usuarios" (
   "id" SERIAL PRIMARY KEY,
   "nombre_usuario" VARCHAR NOT NULL,
   "email" VARCHAR NOT NULL,
   "password" VARCHAR NOT NULL,
-  "dt_creado" DATETIME DEFAULT 'CURRENT_TIMESTAMP',
+  "dt_creado" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   "admin" INT DEFAULT 0,
   "parte_equipo" INT DEFAULT 0
 );
 
-DROP TABLE "servicios" IF EXISTS;
 CREATE TABLE "servicios" (
   "id" SERIAL PRIMARY KEY,
   "srv_nom" VARCHAR NOT NULL,
   "srv_desc" VARCHAR,
-  "srv_precio" NUMBER NOT NULL,
+  "srv_precio" NUMERIC NOT NULL,
   "srv_imagen" BYTEA
 );
 
-DROP TABLE "compras" IF EXISTS;
 CREATE TABLE "compras" (
   "id" SERIAL PRIMARY KEY,
-  "dt_compra" datetime DEFAULT 'CURRENT_TIMESTAMP',
+  "dt_compra" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   "cantidad" INT DEFAULT 0,
-  "id_servicio" INT REFERENCES servicios(id),
-  "id_usuario" INT REFERENCES usuarios(id)
+  "id_servicio" INT REFERENCES "servicios"("id"),
+  "id_usuario" INT REFERENCES "usuarios"("id")
 );
 
-DROP TABLE "compras_servicios" IF EXISTS;
 CREATE TABLE "compras_servicios" (
-  "compras_id_servicio" INT REFERENCES compras(id_servicio),
-  "servicios_id" INT REFERENCES servicios(id),
-  PRIMARY KEY ("compras_id_servicio", "servicios_id")
+  "compras_id_servicio" INT,
+  "servicios_id" INT,
+  PRIMARY KEY ("compras_id_servicio", "servicios_id"),
+  FOREIGN KEY ("compras_id_servicio") REFERENCES "compras"("id"),
+  FOREIGN KEY ("servicios_id") REFERENCES "servicios"("id")
 );
