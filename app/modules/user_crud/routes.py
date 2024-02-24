62aea0b1fbcaa7872b368a1b1e2acf9d9406c58f
brandoncf09 <camps.wp@outlook.com>
2024-02-24 01:35:25 -0600

    .
    Avance

--- app/modules/user_crud/routes.py
+++ app/modules/user_crud/routes.py
@@ -5,3 +5,7 @@ mod = Blueprint('user_crud', __name__)
 @mod.route('/')
 def user_crud_template():
     return render_template('usuario-CRUD.html')
+
+@mod.route('/')
+def user_crud_template():
+    return render_template('productos-CRUD.html')
